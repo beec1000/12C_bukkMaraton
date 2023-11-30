08c7c4f1a14001dc98b4b7e9be6a5743df3410c8
beec1000 <beec1000.minecraft@gmail.com>
2023-11-30 09:32:17 +0100

    Add project files.

---
+++ bukkMaraton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bukkMaraton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzo = new List<Versenyzo>();
+
+            using var sr = new StreamReader(@"..\..\..\src\bukkm2019.txt");
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {          
+                versenyzo.Add(new Versenyzo(sr.ReadLine()));
+            }
+
+            Console.WriteLine("4. feladat");
+            var f4 = 691f - versenyzo.Count;
+            Console.WriteLine($"Versenytávot nem teljesítők: {(f4/ 691) * 100}%");
+
+            Console.WriteLine("5. feladat");
+
+            Console.WriteLine("6. feladat");
+            var f6 = versenyzo.FirstOrDefault(d => d.Ido.Hours >= 6 && d.Ido.Minutes > 0 || d.Ido.Seconds > 0);
+            if (f6 != null)
+            {
+                Console.WriteLine("Volt ilyen versenyző");
+            }
+            else 
+            {
+                Console.WriteLine("Nem volt ilyen versenyző");
+            }
+            
+
+        }
+    }
+}
---
+++ bukkMaraton/Versenyzo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bukkMaraton
+{
+    class Versenyzo
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+
+        public string Tav => _tav.Tav;
+        private Versenytav _tav;
+
+        public Versenyzo(string s)
+        {
+             var v = s.Split(';');
+            this.Rajtszam = v[0];
+            this.Kategoria = v[1];
+            this.Nev = v[2];
+            this.Egyesulet = v[3] == "" ? null : v[3];
+            var ido = v[4].Split(':');
+            this.Ido = new TimeSpan(
+                hours: int.Parse(ido[0]), 
+                minutes: int.Parse(ido[1]), 
+                seconds: int.Parse(ido[2]));
+            this._tav = new Versenytav(this.Rajtszam);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Rajtszam}, {this.Kategoria}, {this.Nev}, {this.Egyesulet}, {this.Ido}";
+        }
+    }
+}
