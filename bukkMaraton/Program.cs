9bc8032def7ed915e61dcc85bf06687583655d06
beec1000 <beec1000.minecraft@gmail.com>
2023-12-03 22:46:25 +0100

    update

--- bukkMaraton/Program.cs
+++ bukkMaraton/Program.cs
@@ -10,7 +10,7 @@ namespace bukkMaraton
     {
         static void Main(string[] args)
         {
-            var versenyzo = new List<Versenyzo>();
+            var versenyzok = new List<Versenyzo>();
 
             using var sr = new StreamReader(@"..\..\..\src\bukkm2019.txt");
 
@@ -18,17 +18,19 @@ namespace bukkMaraton
 
             while (!sr.EndOfStream)
             {          
-                versenyzo.Add(new Versenyzo(sr.ReadLine()));
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
             }
 
             Console.WriteLine("4. feladat");
-            var f4 = 691f - versenyzo.Count;
+            var f4 = 691f - versenyzok.Count;
             Console.WriteLine($"Versenytávot nem teljesítők: {(f4/ 691) * 100}%");
 
             Console.WriteLine("5. feladat");
+            var f5 = versenyzok.Where(d => d.Kategoria.Last() == 'n' && d.Tav == "Rövid").Count();
+            Console.WriteLine($"Női versenyzők száma a rövid távú versenyen: {f5}fő");
 
             Console.WriteLine("6. feladat");
-            var f6 = versenyzo.FirstOrDefault(d => d.Ido.Hours >= 6 && d.Ido.Minutes > 0 || d.Ido.Seconds > 0);
+            var f6 = versenyzok.FirstOrDefault(d => d.Ido.Hours >= 6 && d.Ido.Minutes > 0 || d.Ido.Seconds > 0);
             if (f6 != null)
             {
                 Console.WriteLine("Volt ilyen versenyző");
@@ -37,8 +39,17 @@ namespace bukkMaraton
             {
                 Console.WriteLine("Nem volt ilyen versenyző");
             }
-            
 
+            Console.WriteLine("7. feladat");
+            var f7 = versenyzok.Where(v => v.Kategoria == "ff" && v.Tav == "Rövid").MinBy(v => v.Ido);
+            Console.WriteLine($"A felnőtt férfi (ff) kategória győztese rövid távon \n\t Rajtszám: {f7.Rajtszam} \n\t Név: {f7.Nev} \n\t Egyesület: {f7.Egyesulet} \n\t Idő: {f7.Ido}");
+
+            Console.WriteLine("8. feladat \nStatisztika:");
+            var f8 = versenyzok.Where(d => d.Kategoria.Last() == 'f').GroupBy(d => d.Kategoria).ToDictionary(d => d.Key, dd => dd.Count());
+            foreach (var i in f8)
+            {
+                Console.WriteLine($"{i.Key} - {i.Value}fő");
+            }
         }
     }
 }
